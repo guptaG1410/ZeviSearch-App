7139ef336539b810c9344f14d005c750ca27b24e
Govind Gupta <guptagovind1410@gmail.com>
2023-07-28 22:30:07 +0530

    Added Latest Trends Modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../components/SearchBar';
+import LatestTrends from '../components/LatestTrends';
 import zeviLogo from '../assets/zeviLogo.png';
 import styles from './home.module.scss';
 
 const Home = () => {
+  const [isTrends, setIsTrends] = useState(false);
+
   return (
     <div className={`${styles.mainBg}`}>
       <img className={`${styles.zeviLogo}`} src={zeviLogo} alt="zevi-logo" />
-      <div className={`${styles.flexPosition}`}>
+      <div
+        className={`${styles.flexPosition}`}
+        onClick={() => setIsTrends(!isTrends)}
+      >
         <SearchBar />
+        {isTrends && <LatestTrends />}
       </div>
     </div>
   );
